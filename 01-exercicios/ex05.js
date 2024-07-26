93ae5b9482090a2f72770a7a68119446adbaf530
ailtonhs <ailtonhenrick@outlook.com>
2024-07-26 10:13:09 -0300

    Desafio das mensagens

---
+++ 01-exercicios/ex05.js
@@ -0,0 +1,8 @@
+let text = 'JavaScript';
+
+console.log(text.length); //tamanho da strings
+
+console.log(text.toUpperCase()); // transforma em letra maiúsculas
+
+console.log(text.toLowerCase()); //tudo para minúscula
+
