55823456f08c9c8f59cd4dba1e670bef5e926357
ailtonhs <ailtonhenrick@outlook.com>
2024-07-25 21:21:42 -0300

    Variáveis e tipos primitivos

---
+++ 01-exercicios/ex002.js
@@ -0,0 +1,19 @@
+var n = 200;
+
+console.log(typeof n);
+
+n = 'Google';
+
+console.log(typeof n);
+
+n = true
+
+console.log(typeof n);
+
+n = null;
+
+console.log(typeof n);
+
+n = undefined;
+
+console.log(typeof n);
