0d131505e0c1f3927cfb8cc245d24f29867101c0
ailtonhs <ailtonhenrick@outlook.com>
2024-07-26 21:06:23 -0300

    Operadores (parte 1)

---
+++ 01-exercicios/ex007.js
@@ -0,0 +1,59 @@
+//Ordem de precedência
+
+/*
+primerio () = parêntese
+
+segundo ** = potência
+
+terceiro * / % = multiplicação, divisão, resto da divisão 
+
+quarto + - = adição , subtração
+
+*/
+
+
+//simplificações
+
+/*
+var n = 3
+
+n = n + 4 simplificado n += 4
+
+n = n - 5 simplificado n -= 5
+
+n = n * 4 simplificado n *= 5
+
+n = n / 2 simplificado n /= 2
+
+n = n ** 2 simplificado n **= 2
+
+n = n % 5 simplificado n %= 2
+
+*/
+
+var num = 8 ;
+num += 2; //10
+num -= 5; //5
+num *= 5; //25
+num /= 2; //12.5
+num **= 2 // 156.25
+num %= 2// 0.25
+
+
+console.log(num);
+
+//incremento
+
+/*
+var x = 5;
+x = x + 1; simplificado x++
+x = x - 1; simplificado x--
+
+
+*/
+
+var n = 10;
+n ++; //11
+n --; //10
+
+console.log(n);
