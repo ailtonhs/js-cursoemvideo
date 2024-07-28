b29541c8f9e5b93ea3df89b7871e8957db9dbd43
ailtonhs <ailtonhenrick@outlook.com>
2024-07-27 21:49:56 -0300

    Condições (parte 1)

---
+++ 01-exercicios/ex012.js
@@ -0,0 +1,21 @@
+//condições
+
+/*
+condição simples
+if(condição) {
+    executa quando é true
+};
+
+    
+*/
+
+let vel = 60.5;
+
+
+console.log(`A velocidade do seu carro é ${vel}!`);
+
+if (vel > 60) {
+    console.log('Você ultrapasou a velocidade permitida. MULTADO!');
+}
+
+console.log(`Dirija sempre com cinto de segurança!`);
---
+++ 01-exercicios/ex013.js
@@ -0,0 +1,22 @@
+/*
+Condição composta
+
+if(condicão) {
+    executa quando for true
+
+} else {
+    executa quando for false
+ }
+
+*/
+
+let pais = 'Brasil';
+
+if (pais === 'Brasil') {
+    console.log('Brasileira');
+
+} else {
+    console.log('Estrangeiro');
+}
+
+console.log(`Vivendo em ${pais}`);
