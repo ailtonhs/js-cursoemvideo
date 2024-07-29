217a1b990ac20d8c07d8455ddc61e050636c406c
ailtonhs <ailtonhenrick@outlook.com>
2024-07-28 21:22:47 -0300

    Condições (parte 2)

---
+++ 01-exercicios/ex017.js
@@ -0,0 +1,13 @@
+let horas = new Date().getHours()
+
+console.log(`Agora são exatamente ${horas} horas`);
+
+if (horas < 12) {
+    console.log('Bom Dia!');
+
+} else if (horas <= 18) {
+    console.log('Boa Tarde!');
+
+} else {
+    console.log('Boa Noite');
+}
